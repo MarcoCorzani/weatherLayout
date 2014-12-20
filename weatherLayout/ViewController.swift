36c0eba9533658a2bf4229b35aa5782c3fdae96e
Marco <Marco@Marcos-iMac.fritz.box>
2014-12-20 11:23:21 +0100

    Initial Commit

---
+++ weatherLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weatherLayout
+//
+//  Created by Marco F.A. Corzani on 20.12.14.
+//  Copyright (c) 2014 Alphaweb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
